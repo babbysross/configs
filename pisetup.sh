8557693cc6b3151931683969c980932cbb86ec3d
babbysross <rsg.elias@gmail.com>
2018-04-19 13:16:07 +0000

    Moved a wallpaper and updated pisetup accordingly.

--- pisetup.sh
+++ pisetup.sh
@@ -7,7 +7,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 apt-get update -y && apt-get upgrade -y
-apt-get install -my guake terminator neovim emacs zsh i3lock ntpdate gparted exfat-fuse exfat-utils
+apt-get install -my guake terminator neovim emacs zsh i3lock ntpdate gparted exfat-fuse exfat-utils neofetch
 ntpdate uk.pool.ntp.org
 mkdir /tmp/deleteme && cd /tmp/deleteme
 wget -P /tmp/deleteme https://pcaro.es/d/otf-hermit-1.21.tar.gz
@@ -25,4 +25,4 @@ cp ~/Projects/configs/.zshrc ~/.zshrc
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 cp ~/Projects/configs/wallpapers/PiBackgroundITER.png /usr/share/rpd-wallpaper/
 sed -i 's/cliff.jpg/PiBackgroundITER.png/g' ~/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
-cp ~/Projects/configs/Stars-at-night.png ~/Pictures
+cp ~/Projects/configs/wallpapers/Stars-at-night.png ~/Pictures
