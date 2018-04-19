b3619aef37dde0e7a2cf56fdc6cfaa40c48fdc2c
babbysross <rsg.elias@gmail.com>
2018-04-19 12:54:04 +0000

    updated pisetup

--- pisetup.sh
+++ pisetup.sh
@@ -7,7 +7,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 apt-get update -y && apt-get upgrade -y
-apt-get install -my guake terminator neovim emacs zsh i3lock ntpdate gparted
+apt-get install -my guake terminator neovim emacs zsh i3lock ntpdate gparted exfat-fuse exfat-utils
 ntpdate uk.pool.ntp.org
 mkdir /tmp/deleteme && cd /tmp/deleteme
 wget -P /tmp/deleteme https://pcaro.es/d/otf-hermit-1.21.tar.gz
@@ -17,10 +17,12 @@ cp Hermit-light.otf /usr/share/fonts/truetype/
 cp Hermit-medium.otf /usr/share/fonts/truetype/
 cd ~ && rm -rf /tmp/deleteme
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 curl -sLf https://spacevim.org/install.sh | bash
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 git clone https://github.com/babbysross/configs ~/Projects/configs && cd ~/Projects/configs
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ~/Projects/configs/.zshrc ~/.zshrc
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+cp ~/Projects/configs/wallpapers/PiBackgroundITER.png /usr/share/rpd-wallpaper/
+sed -i 's/cliff.jpg/PiBackgroundITER.png/g' ~/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
 cp ~/Projects/configs/Stars-at-night.png ~/Pictures
